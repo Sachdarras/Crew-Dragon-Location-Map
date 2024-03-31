84d5c4d234e4115ec44dbd8f4060754307309dea
Sachdarras <sachdarras@gmail.com>
2024-03-31 10:40:21 +0200

    gitignore

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
-const crewDragon=[
-    {
-        Nom:"Darras",
-        Prenom:"Sacha",
-       latitude:"48.0380024",
-       longitude:"-4.4852118",
-       
-    
-    },
+import crewDragon from './CD.js';
 
-    {
-     Nom:"Herpoux",
-     Prenom:"Franck",
-     latitude: "43.656806",
-     longitude: "1.277952",
-    },
-    {Nom:"Gorski",
-    Prenom:"Anthony",
-    latitude: "43.3304705",
-    longitude: "5.4496847",
-    },
-    {
 
-    },
-    ]
     
     function initMap() {
         const map = L.map('map').setView([46.603354, 1.888334], 6);
