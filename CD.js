d8568b13b55eaf207b980922a5dd1909bbbaffd7
Sachdarras <sachdarras@gmail.com>
2024-04-02 09:39:16 +0200

    MAJ+ FAB+MOMO

--- CD.js
+++ CD.js
@@ -1,70 +1,77 @@
-const crewDragon=[
-    {
-        Nom:"Darras",
-        Prenom:"Sacha",
-       latitude:"48.0380024",
-       longitude:"-4.4852118",
-       
-    
-    },
+const crewDragon = [
+  {
+    Nom: "Darras",
+    Prenom: "Sacha",
+    latitude: "48.0380024",
+    longitude: "-4.4852118",
+  },
 
-    {
-     Nom:"Herpoux",
-     Prenom:"Franck",
-     latitude: "43.656806",
-     longitude: "1.277952",
-    },
-    {Nom:"Gorski",
-    Prenom:"Anthony",
+  {
+    Nom: "Herpoux",
+    Prenom: "Franck",
+    latitude: "43.656806",
+    longitude: "1.277952",
+  },
+  {
+    Nom: "Gorski",
+    Prenom: "Anthony",
     latitude: "43.3304705",
     longitude: "5.4496847",
-    },
-    {Nom:"Pasquiou",
-    Prenom:"Vincent",
+  },
+  {
+    Nom: "Pasquiou",
+    Prenom: "Vincent",
     latitude: "48.732084",
     longitude: "-3.459144",
-    },
+  },
 
-{
-    Nom:"Lanzetti",
-Prenom:"David",
-latitude: " 49.601",
-longitude: " 4.423",
-},
-{
-    Nom:"Prêtre",
-Prenom:"Fanny",
-latitude: " 46.160540",
-longitude: "-1.151193",
-
-},
-{
-    Nom:"Bauduin",
-Prenom:"Laurent",
-latitude: "50.437008",
-longitude: "3.432927",
-
-},
-{
-    Nom:"Besch",
-Prenom:"Simon",
-latitude: "47.38991722684452",
-longitude: "0.688728222980062",
-
-},
-{
-    Nom:"Chetot",
-Prenom:"Aurélien",
-latitude: "46.8001341",
-longitude: "4.858671",
-
-},
-{
-    Nom:"Etchart",
-Prenom:"Amandine",
-latitude: "44.973966488075774",
-longitude: " -0.1848856298805242",
-
-},
- ];
-    export default crewDragon;
+  {
+    Nom: "Lanzetti",
+    Prenom: "David",
+    latitude: " 49.601",
+    longitude: " 4.423",
+  },
+  {
+    Nom: "Prêtre",
+    Prenom: "Fanny",
+    latitude: " 46.160540",
+    longitude: "-1.151193",
+  },
+  {
+    Nom: "Bauduin",
+    Prenom: "Laurent",
+    latitude: "50.437008",
+    longitude: "3.432927",
+  },
+  {
+    Nom: "Besch",
+    Prenom: "Simon",
+    latitude: "47.38991722684452",
+    longitude: "0.688728222980062",
+  },
+  {
+    Nom: "Chetot",
+    Prenom: "Aurélien",
+    latitude: "46.8001341",
+    longitude: "4.858671",
+  },
+  {
+    Nom: "Etchart",
+    Prenom: "Amandine",
+    latitude: "44.973966488075774",
+    longitude: " -0.1848856298805242",
+  },
+  {
+    Nom: "Laquerriere",
+    Prenom: "Fabien",
+    latitude: "49.207308",
+    longitude: "1.185249",
+  },
+  {
+    Nom: "Jebri",
+    Prenom: "Mohamed",
+    latitude: "43.398953",
+    longitude: "5.366397",
+  },
+];
+export default crewDragon;
